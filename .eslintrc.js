1f8cf1747d354c8bc01eb99385430925a6aaed73
Nando-DevOps <luiz.moreira1@pucpr.edu.br>
2026-04-09 12:45:58 -0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
 // GUIA ESLINT: https://eslint.org/docs/latest/use/configure/migration-guide
 // .eslintrc.js
 
-module.exports = {
-  // ...other config
-  plugins: ["jsdoc"],
-  rules: {
-    "jsdoc/require-description": "error",
-    "jsdoc/check-values": "error",
-  },
-  // ...other config
+export const plugins = ["jsdoc"];
+export const rules = {
+	"jsdoc/require-description": "error",
+	"jsdoc/check-values": "error",
 };
