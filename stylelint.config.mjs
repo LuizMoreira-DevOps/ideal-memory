da75b2342d20c0414ac23adc5d483fd2cfc1801a
Nando-DevOps <luiz.moreira1@pucpr.edu.br>
2026-04-10 09:13:25 -0300

    Commit

---
+++ stylelint.config.mjs
@@ -0,0 +1,11 @@
+// stylelint configuration file
+// https://stylelint.io/user-guide/configure/
+
+// stylelint.config.mjs
+export default {
+  rules: {
+    "color-no-invalid-hex": true,
+    "font-family-no-missing-generic-family-keyword": true,
+    "no-descending-specificity": null,
+  },
+};
