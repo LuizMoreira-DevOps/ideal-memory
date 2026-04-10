57e29f8ad46d7862d39e95b6236be87c5385b840
Nando-DevOps <luiz.moreira1@pucpr.edu.br>
2026-04-10 09:05:50 -0300

    commit

--- stylelint.confg.mjs
+++
@@ -1,6 +0,0 @@
-// stylelint configuration file
-// https://stylelint.io/user-guide/configure/
-
-export default {
-  extends: ["stylelint-config-standard"],
-};
---
+++ stylelint.config.cjs
@@ -0,0 +1,10 @@
+// stylelint configuration file
+// https://stylelint.io/user-guide/configure/
+
+// stylelint.config.cjs
+module.exports = {
+  rules: {
+    "block-no-empty": true,
+    "color-no-invalid-hex": true,
+  },
+};
