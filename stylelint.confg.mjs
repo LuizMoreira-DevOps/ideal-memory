3a94ddbaa04deeb49b15e5b07987f44cfe7d29c9
Nando-DevOps <luiz.moreira1@pucpr.edu.br>
2026-04-10 09:00:54 -0300

    Ajustes

--- stylelint.confg.mjs
+++ stylelint.confg.mjs
@@ -2,9 +2,5 @@
 // https://stylelint.io/user-guide/configure/
 
 export default {
-  rules: {
-    "block-no-empty": true,
-    "color-no-invalid-hex": true,
-    "declaration-block-no-duplicate-properties": true,
-  },
+  extends: ["stylelint-config-standard"],
 };
