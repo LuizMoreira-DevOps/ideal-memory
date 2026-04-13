d2d52f009829da628eb2d33851c77e748b925844
Nando-DevOps <luiz.moreira1@pucpr.edu.br>
2026-04-13 15:57:06 -0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI # Importação do FastAPI para criar a aplicação web
 import random
+from fastapi import FastAPI # Importação do FastAPI para criar a aplicação web
 from pydantic import BaseModel # Importação do BaseModel para criar modelos de dados
+from fastapi.staticfiles import StaticFiles # Importação do StaticFiles para servir arquivos estáticos, como HTML, CSS e JavaScript
 
 app = FastAPI() # Criação da instância do FastAPI para a aplicação web
+
+# Montagem do diretório "assets" para servir arquivos estáticos, como HTML, CSS e JavaScript
+app.mount("/", StaticFiles(directory="assets", html=True), name="assets")
 
 # Modelo de dados para um estudante
 class Estudante(BaseModel):
@@ -11,7 +15,7 @@ class Estudante(BaseModel):
     ativo: bool
 
 
-@app.get("/")
+@app.get("/hello")
 def read_root():
     return {"Hello": "World"}
 
